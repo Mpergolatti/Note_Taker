a3a87e945f8f750bc0569a6d55e56e52c782d8d8
Mpergolatti <Mpergolatti@gmail.com>
2022-10-24 14:44:10 -0400

    working on adding notes and getting notes

---
+++ db/store.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const fs = require("fs");
+
+class Store {
+  read() {
+      return readFileAsync("db/db.json", "utf8")
+  }
+  write(note) {
+      return writeFileAsync("db/db.json", JSON.stringify(note))
+  }
+  addNote(note) {
+      const { title, text } = note
+      if (!title || !text) {
+          throw new Error("title and text cannot be blank")
+      }
+      const newNote = { title, text, id: uuid() }
+      return this.getNotes()
+          .then(notes => [...notes, newNote])
+          .then(updatedNotes => this.write(updatedNotes))
+          .then(() => this.newNote)
+  }
+  getNotes() {
+      return this.read()
+          .then(notes => {
+              return JSON.parse(notes) || [];
+          })
+  }
+
+  module.exports = new Store();
